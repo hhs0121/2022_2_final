0aecc80d5189a184845fe0b6320b0aadab70f9a0
황현성 <93861061+hhs0121@users.noreply.github.com>
2022-12-18 02:45:01 +0900

    Add files via upload

--- cal/forms.py
+++ cal/forms.py
@@ -1,14 +1,9 @@
 from django.forms import ModelForm, DateInput
 from cal.models import Event
 
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
 class EventForm(ModelForm):
   class Meta:
     model = Event
-    # datetime-local is a HTML5 input type, format to make date time show on fields
     widgets = {
       'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
       'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
@@ -17,7 +12,6 @@ class EventForm(ModelForm):
 
   def __init__(self, *args, **kwargs):
     super(EventForm, self).__init__(*args, **kwargs)
-    # input_formats parses HTML5 datetime-local input to datetime field
     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
     self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
 
--- cal/urls.py
+++ cal/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from . import views
 from django.urls import path, include
 
